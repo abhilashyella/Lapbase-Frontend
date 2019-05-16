c8daa83fb7a13de81f1a73d1754c7999d3fbf406
19600033 <19600033@students.latrobe.edu.au>
2019-05-16 15:21:45 +1000

    2nd commit

--- Lapbase Application/Controllers/HomeController.cs
+++ Lapbase Application/Controllers/HomeController.cs
@@ -7,12 +7,14 @@ using System.DirectoryServices.ActiveDirectory;
 using System.DirectoryServices;
 using System.DirectoryServices.AccountManagement;
 using System.DirectoryServices.Protocols;
+using Lapbase_Application.Models;
+
 
 namespace Lapbase_Application.Controllers
 {
     public class HomeController : Controller
     {
-        public  ActionResult Index(String username, String password)
+        public  ActionResult Index()
         {
 
 
@@ -20,59 +22,69 @@ namespace Lapbase_Application.Controllers
             //public bool AuthenticateUser(string username, string password)
             //{
             string domainName = System.Configuration.ConfigurationManager.AppSettings["Domain Name"];
+            string username = System.Configuration.ConfigurationManager.AppSettings["User ID"];
+            string password = System.Configuration.ConfigurationManager.AppSettings["User Password"];
             string domainAndUsername = string.Format(@"{0}\{1}", domainName, username);
             string ldapPath = string.Format("LDAP://" + domainName);
-            bool authentic = false;
-            //this.usermustchangepassword = false;
-            //this.useraccountisexpired = false;
-            //txttest.value = "";
+            test t1 = new test();
+       
+          //this.usermustchangepassword = false;
+          //this.useraccountisexpired = false;
+          //txttest.value = "";
+
 
             try
 
             {
 
-                DirectoryEntry entry = new DirectoryEntry(ldapPath, domainAndUsername, password);
+               
+                        DirectoryEntry entry = new DirectoryEntry(ldapPath, username, password);
 
-                using (PrincipalContext context = new PrincipalContext(ContextType.Domain, domainName))
+                        //using (PrincipalContext context = new PrincipalContext(ContextType.Domain, domainName))
 
-                {
+                        //{
 
-                    UserPrincipal userPrincipal = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, username);
+                        //    UserPrincipal userPrincipal = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, username);
 
-                    //if (userPrincipal.AccountExpirationDate.HasValue)
+                        //    //if (userPrincipal.AccountExpirationDate.HasValue)
 
-                    //    if (userPrincipal.AccountExpirationDate < DateTime.Now)
+                        //    //    if (userPrincipal.AccountExpirationDate < DateTime.Now)
 
-                    //        this.userAccountIsExpired = true;
+                        //    //        this.userAccountIsExpired = true;
 
-                    //if (!userPrincipal.LastPasswordSet.HasValue)
+                        //    //if (!userPrincipal.LastPasswordSet.HasValue)
 
-                    //    if (!userPrincipal.PasswordNeverExpires)
+                        //    //    if (!userPrincipal.PasswordNeverExpires)
 
-                    //        this.userMustChangePassword = true;
+                        //    //        this.userMustChangePassword = true;
+
+                        //}
+
+                        // now authenitcate the user
+
+                        object obj = entry.NativeObject;
+
+                // authentic = true;
+                t1.s = "valid user";
+
+
+                    }
+
+                    catch (Exception ex)
+
+                    {
+
+                        //throw ex;
+                t1.s = ""+ex.ToString();
+                        
+
+                    }
+
+            return View(t1);
 
                 }
 
-                // now authenitcate the user
-
-                object obj = entry.NativeObject;
-
-                authentic = true;
+                
 
             }
-
-            catch (Exception ex)
-
-            {
-
-                throw ex;
-
-            }
-
-
-            return View(authentic);
-            //#endregion
-
-        }
     }
-}
---
+++ Lapbase Application/Models/test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lapbase_Application.Models
+{
+    public class test
+
+    {
+        public String s { get; set; }
+    }
+}
